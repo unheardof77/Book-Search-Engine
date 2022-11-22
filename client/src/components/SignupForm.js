f620f9dcd575d7791a383fd6bc67c0a355de2e44
Morgan Tolman <morgan.tolman04@gmail.com>
2022-11-21 22:09:34 -0700

    Updated Sigin.  Safe revert point.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
+import {useMutation} from '@apollo/client'
+import { CREATEUSER } from '../utils/mutations'
+
 const SignupForm = () => {
+  const [createNewUser, { error }] = useMutation(CREATEUSER)
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -28,15 +32,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await createNewUser({variables: userFormData});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.createNewUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
